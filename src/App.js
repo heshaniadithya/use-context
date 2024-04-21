241fe4311382be784c911cc9eae61a8340975ac4
heshaniadithya <heshaniadithya2006@gmail.com>
2024-04-21 20:50:09 +0530

    use context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "../src/components/Dashboard";
+import Home from "../src/components/Home";
+import Recommendations from "../src/components/Recommendations";
+import Podcasts from "../src/components/Podcasts";
+import { UserContext } from "./contexts/UserContext";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState("Heshani");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <UserContext.Provider value={[user, setUser]}>
+       <Routes>
+        <Route 
+        path="/"
+        element={<Home />}
+        />
+          <Route
+           path="/dashboard" 
+           element={<Dashboard />} 
+          />
+          <Route 
+          path="/recommendations" 
+          element={<Recommendations />} 
+          />
+          <Route 
+          path="/podcasts" 
+          element={<Podcasts />} 
+          />
+       </Routes>
+       </UserContext.Provider>
+    </Router>
+    </>
   );
 }
 
